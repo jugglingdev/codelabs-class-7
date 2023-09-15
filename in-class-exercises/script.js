387f77d524163ebf8a1e107a1e53fa0a5028e8c4
jugglingdev <kaylamarie1785@gmail.com>
2023-09-14 20:14:41 -0500

    Completed Exercise 4

--- in-class-exercises/script.js
+++ in-class-exercises/script.js
@@ -1,4 +1,5 @@
-// Exercise 1
+// Exercises 1 and 4 (updated)
+// Ignore undefined in Exercise 1
 
 class Subject {
     constructor() {
@@ -13,15 +14,36 @@ class Subject {
         this.observers = this.observers.filter((item) => item !== observer);
     }
 
-    notifyObservers() {
+    notifyObservers(data) {
         this.observers.forEach((observer) => {
-            observer.update();
+            observer.update(data);
         });
+    }
+
+    async fetchAndNotify() {
+        const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10';
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log('Exercise 4 Broadcasting...');
+            this.notifyObservers(data);
+
+        } catch {
+            console.log('Exercise 4 Broadcasting...');
+            this.notifyObservers('Something went wrong. :(');
+        }
     }
 }
 
 class Observer {
-    update() {
+    update(data) {
+        if (typeof data == 'object') {
+            const [{ title }] = data;
+            console.log(title);
+        } else {
+            console.log(data);
+        }
         console.log('Observer updated!');
     }
 }
@@ -40,6 +62,10 @@ console.groupCollapsed('Exercise 1');
     subject.notifyObservers();
 
 console.groupEnd();
+
+// Calling function for Exercise 4
+
+subject.fetchAndNotify();
 
 // Exercise 2
 
@@ -71,6 +97,7 @@ console.groupEnd();
 // Exercise 3
 
 console.groupCollapsed('Exercise 3');
+    console.log('List of 10 JSON items below');
 
     async function fetchPosts() {
         const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10';
@@ -88,8 +115,3 @@ console.groupCollapsed('Exercise 3');
     fetchPosts();
 
 console.groupEnd();
-
-// Exercise 4
-
-
-
